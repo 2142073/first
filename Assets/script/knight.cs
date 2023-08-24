7dcb8feff568b2063f0780e5189cbb45174d27c7
2142073 <s2142073VX@s.chibakoudai.jp>
2023-08-24 20:05:28 +0900

    l

--- Assets/script/knight.cs
+++ Assets/script/knight.cs
@@ -6,24 +6,31 @@ public class knight : MonoBehaviour
 {
     int attack = 5;
     int attackRange = 100;
-    float attackInterval = 1.5f; 
-   
+    float attackInterval = 1.5f;
+    bool enemyCheck = false;
+
     void Start()
     {
-        //自分の座標を取得
-        Vector3 pos = transform.position;
-        //周囲１マスのenemyタグを持っている敵を取得
-        Collider[] hitColliders = Physics.OverlapSphere(pos, attackRange);
-        //もし敵がいたら（敵を見つけた）出力する
-        if (hitColliders.Length > 0)
+        //attackRange内にenemyのタグを持つ敵がいるかどうかを確認する
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, attackRange);
+        //enemyのタグを持つ敵がenemyCheckをtrueにする
+        foreach (Collider hitCollider in hitColliders)
         {
-            Debug.Log("敵を見つけた");
+            if (hitCollider.tag == "enemy")
+            {
+                enemyCheck = true;
+            }
+        }
+        if (enemyCheck == true)
+        {
+            Debug.Log("敵がいる");
+
         }
     }
 
 
     void Update()
     {
-        
+
     }
 }
