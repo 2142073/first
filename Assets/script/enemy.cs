d2124dd9f2119671299445fa7459f9b4e792accd
gan <103314509+ganytbs0910@users.noreply.github.com>
2023-09-02 09:07:26 +0900

    動き訂正

--- Assets/script/enemy.cs
+++ Assets/script/enemy.cs
@@ -7,45 +7,52 @@ public class enemy : MonoBehaviour
     public static enemy instance;
     private Rigidbody2D rb;
     private float speed = 30f;
-    public int x=1;
-    public int y=0;
+    public int x = 1;
+    public int y = 0;
 
-    void Awake() 
+    void Awake()
     {
-        instance=this;
+        instance = this;
     }
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        right();
     }
 
     void FixedUpdate()
     {
-                 //毎秒x軸に１ずつ移動する
-    transform.Translate(x * Time.deltaTime, y * Time.deltaTime, 0);
+        //毎秒x軸に１ずつ移動する
+        transform.Translate(x * Time.deltaTime, y * Time.deltaTime, 0);
     }
 
-    public void right()
+    void Update()
+    {
+
+    }
+    public void RightMove()
     {
         Debug.Log("右");
-        x=1;
-        y=0;
+        x = 1;
+        y = 0;
     }
-     public void left()
+    public void LeftMove()
     {
         Debug.Log("左");
-        x=-1;
-        y=0;
-    } public void up()
+        x = -1;
+        y = 0;
+    }
+    public void UpMove()
     {
-        x=0;
-        y=1;
-    } public void down()
+        Debug.Log("上");
+        x = 0;
+        y = 1;
+    }
+    public void DownMove()
     {
-        x=0;
-        y=-1;
+        Debug.Log("下");
+        x = 0;
+        y = -1;
     }
 
 }
